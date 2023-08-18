94e87d1a1347a49bded0bc01cdeba6923494cafd
manish <manishpanwar682@gmail.com>
2023-08-18 04:48:09 -1200

    hello

--- Schema/userSchema.js
+++ Schema/userSchema.js
@@ -39,24 +39,3 @@ const userSchema = new mongoose.Schema(
 const user =  mongoose.model("User", userSchema);
 
 module.exports = user;
-// const mongoose = require("mongoose")
-
-// const UserSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         min: 6,
-//     },
-// }, {timestamps: true})
-
-// module.exports = mongoose.model("User", UserSchema)
